a8172e0e33253d25ce250414e4f815be68a06164
Rahul Kumar <rk7870382701@gmail.com>
2020-11-28 19:17:32 +0530

    Comment updated

--- IndianFlag/src/com/flag/Main.java
+++ IndianFlag/src/com/flag/Main.java
@@ -3,7 +3,7 @@ package com.flag;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("----Started----");
+		System.out.println("--Program--Started----");
 		new MyFrame();
 	}
 
